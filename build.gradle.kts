232df9e2e2e2819aef66e2b32755f655927acb50
mavrikis-kostas <mavrikis.kostas@moneyforward.co.jp>
2023-12-21 16:46:16 +0900

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val kotlin_logging_version: String by project
+val kotlinx_datetime_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("io.ktor.plugin") version "2.3.6"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:$kotlinx_datetime_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.github.oshai:kotlin-logging-jvm:$kotlin_logging_version")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,20 @@
+package com.example
+
+import com.example.plugins.*
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+val logger = KotlinLogging.logger {}
+
+fun main() {
+    logger.info { "Starting server" }
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
